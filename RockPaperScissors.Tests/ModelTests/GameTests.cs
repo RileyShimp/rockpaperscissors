dcab29bf4611e402ddb30c063dcf36d43408e02d
Riley Shimp <rilryshimp@gmail.com>
2022-02-23 14:08:34 -0800

    Add code to pass fourth test
    co-authored-by: Matt Eilar <meilar@gmail.com>

--- RockPaperScissors.Tests/ModelTests/GameTests.cs
+++ RockPaperScissors.Tests/ModelTests/GameTests.cs
@@ -29,5 +29,14 @@ namespace RockPaperScissors.Tests
       Assert.AreEqual("Player 1 wins!", results);
       Assert.AreEqual("Player 2 wins!", results2);
     }
+
+    [TestMethod]
+    public void GameLogic_Handles3s_String()
+    {
+      string results = Game.GameLogic("3","1"); 
+      string results2 = Game.GameLogic("1","3");
+      Assert.AreEqual("Player 2 wins!", results);
+      Assert.AreEqual("Player 1 wins!", results2);
+    }
   }
 }
--- RockPaperScissors/Models/Game.cs
+++ RockPaperScissors/Models/Game.cs
@@ -6,22 +6,32 @@ namespace RockPaperScissors
     {
       int value1 = int.Parse(player1);
       int value2 = int.Parse(player2);
+      string win1 = "Player 1 wins!";
+      string win2 = "Player 2 wins!";
 
       if (value1 == value2) 
       {
         return "Player 1 and Player 2 tie.";
       } 
+      else if (value1 == 3 && value2 == 1)
+      {
+        return win2;
+      }
+      else if (value1 == 1 && value2 == 3)
+      {
+        return win1;
+      }
       else if (value1 > value2)
       {
-        return "Player 1 wins!";
+        return win1;
       }
       else if (value2 > value1)
       {
-        return "Player 2 wins!";
+        return win2;
       }
       else 
       {
-        return "Nobody wins";
+        return "Error : Please enter number between 1 and 3";
       }
     }
   }
